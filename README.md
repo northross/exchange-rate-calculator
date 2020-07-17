e53e9435eb384cc28def3e3fbd74a14ad17df3c2
northross <rossnorthrup@yahoo.com>
2020-07-17 13:41:26 -0400

    republishing

---
+++ README.md
@@ -0,0 +1,14 @@
+# Exchange Rate Calculator
+
+### This project was created for the practice of utilizing a 3rd party API.
+
+#### This Exchange Rate Calculator:
+
+- provides inputs and outputs
+- calculates exchange rates
+- swaps values
+- tests for valid input and output
+
+## Exchange Rate Calculator
+
+<img src="https://imgur.com/1Vx8Eco.png" width="800">
---
+++ exchange-rate-calculator/script.js
@@ -0,0 +1,48 @@
+const currencyEl_one = document.getElementById('currency-one');
+const amountEl_one = document.getElementById('amount-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_two = document.getElementById('amount-two');
+
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+// Fetch exchance rates and update the dom
+
+function calculate() {
+  const currency_one = currencyEl_one.value;
+  const currency_two = currencyEl_two.value;
+  fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
+    .then((res) => res.json())
+    .then((data) => {
+      //   console.log(data.result);
+      if (data.result === 'error') {
+        rateEl.innerText = `No data for ${currency_one}`;
+        amountEl_one.value = '';
+        amountEl_two.value = '';
+      } else {
+        const rate = data.rates[currency_two];
+        if (rate) {
+          rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+          amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+        } else {
+          rateEl.innerText = `No data for ${currency_two}`;
+          amountEl_one.value = '';
+          amountEl_two.value = '';
+        }
+      }
+    });
+}
+
+// event listeners
+currencyEl_one.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+currencyEl_two.addEventListener('change', calculate);
+amountEl_two.addEventListener('input', calculate);
+
+swap.addEventListener('click', () => {
+  const temp = currencyEl_one.value;
+  currencyEl_one.value = currencyEl_two.value;
+  currencyEl_two.value = temp;
+  calculate();
+});
+calculate();
